76a1c3c0ac5c612651bef03b9671f4a2bb2f1488
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-10-15 23:44:05 -0300

    agrego lista

---
+++ app/src/main/java/com/example/recyclerviewexample/SuperHeroProvider.kt
@@ -0,0 +1,74 @@
+package com.example.recyclerviewexample
+
+class SuperHeroProvider {
+    companion object {
+        val superHeroList = listOf<SuperHero>(
+            SuperHero(
+                "Spiderman",
+                "Marvel",
+                "Peter Parker",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/spiderman.jpg"
+            ),
+            SuperHero(
+                "Batman",
+                "DC",
+                "Bruce Wayne",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/batman.jpg"
+            ),
+            SuperHero(
+                "Thor",
+                "Marvel",
+                "Thor Odinson",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/thor.jpg"
+            ),
+            SuperHero(
+                "Flash",
+                "DC",
+                "Jay Garrick",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/flash.png"
+            ),
+            SuperHero(
+                "Daredevil",
+                "Marvel",
+                "Matthew Michael Murdock",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/daredevil.jpg"
+            ),
+            SuperHero(
+                "Wonder Woman",
+                "DC",
+                "Princess Diana",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/wonder_woman.jpg"
+            ),
+            SuperHero(
+                "Green Arrow",
+                "DC",
+                "Oliver Queen",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/green_arrow.jpg"
+            ),
+            SuperHero(
+                "Batgirl",
+                "DC",
+                "Barbara Gordon",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/batgirl.jpg"
+            ),
+            SuperHero(
+                "Superwoman",
+                "DC",
+                "Kara Zor-El",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/superwoman.jpg"
+            ),
+            SuperHero(
+                "Green Lantern",
+                "DC",
+                "Alan Scott",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/green-lantern.jpg"
+            ),
+            SuperHero(
+                "Wolverine",
+                "Marvel",
+                "James Howlett",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/logan.jpeg"
+            )
+        )
+    }
+}
