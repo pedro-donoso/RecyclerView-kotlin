b471801722a4c9162cd0ab87f47d94753f7dc3d3
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-10-17 01:08:15 -0300

    agrego formato imagen

--- app/src/main/java/com/example/recyclerviewexample/SuperHeroProvider.kt
+++ app/src/main/java/com/example/recyclerviewexample/SuperHeroProvider.kt
@@ -40,24 +40,6 @@ class SuperHeroProvider {
                 "https://cursokotlin.com/wp-content/uploads/2017/07/wonder_woman.jpg"
             ),
             SuperHero(
-                "Green Arrow",
-                "DC",
-                "Oliver Queen",
-                "https://cursokotlin.com/wp-content/uploads/2017/07/green_arrow.jpg"
-            ),
-            SuperHero(
-                "Batgirl",
-                "DC",
-                "Barbara Gordon",
-                "https://cursokotlin.com/wp-content/uploads/2017/07/batgirl.jpg"
-            ),
-            SuperHero(
-                "Superwoman",
-                "DC",
-                "Kara Zor-El",
-                "https://cursokotlin.com/wp-content/uploads/2017/07/superwoman.jpg"
-            ),
-            SuperHero(
                 "Green Lantern",
                 "DC",
                 "Alan Scott",
@@ -68,6 +50,18 @@ class SuperHeroProvider {
                 "Marvel",
                 "James Howlett",
                 "https://cursokotlin.com/wp-content/uploads/2017/07/logan.jpeg"
+            ),
+            SuperHero(
+                "Cyclops",
+                "Marvel",
+                "Scott Summers",
+                "https://i.pinimg.com/originals/24/a9/50/24a9503b4d68245584767b36c1e04202.jpg"
+            ),
+            SuperHero(
+                "Green Arrow",
+                "DC",
+                "Oliver Queen",
+                "https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:600/h:442/q:75/https://bleedingcool.com/wp-content/uploads/2018/03/AR616b_0344b.jpg"
             )
         )
     }
