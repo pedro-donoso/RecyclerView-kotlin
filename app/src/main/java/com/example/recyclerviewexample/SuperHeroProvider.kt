ff7724ddedcd374bf3f7ca92411c6dbd595c37d6
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-10-17 01:24:42 -0300

    imagen orden

--- app/src/main/java/com/example/recyclerviewexample/SuperHeroProvider.kt
+++ app/src/main/java/com/example/recyclerviewexample/SuperHeroProvider.kt
@@ -4,52 +4,10 @@ class SuperHeroProvider {
     companion object {
         val superHeroList = listOf<SuperHero>(
             SuperHero(
-                "Spiderman",
-                "Marvel",
-                "Peter Parker",
-                "https://cursokotlin.com/wp-content/uploads/2017/07/spiderman.jpg"
-            ),
-            SuperHero(
                 "Batman",
                 "DC",
                 "Bruce Wayne",
-                "https://cursokotlin.com/wp-content/uploads/2017/07/batman.jpg"
-            ),
-            SuperHero(
-                "Thor",
-                "Marvel",
-                "Thor Odinson",
-                "https://cursokotlin.com/wp-content/uploads/2017/07/thor.jpg"
-            ),
-            SuperHero(
-                "Flash",
-                "DC",
-                "Jay Garrick",
-                "https://cursokotlin.com/wp-content/uploads/2017/07/flash.png"
-            ),
-            SuperHero(
-                "Daredevil",
-                "Marvel",
-                "Matthew Michael Murdock",
-                "https://cursokotlin.com/wp-content/uploads/2017/07/daredevil.jpg"
-            ),
-            SuperHero(
-                "Wonder Woman",
-                "DC",
-                "Princess Diana",
-                "https://cursokotlin.com/wp-content/uploads/2017/07/wonder_woman.jpg"
-            ),
-            SuperHero(
-                "Green Lantern",
-                "DC",
-                "Alan Scott",
-                "https://cursokotlin.com/wp-content/uploads/2017/07/green-lantern.jpg"
-            ),
-            SuperHero(
-                "Wolverine",
-                "Marvel",
-                "James Howlett",
-                "https://cursokotlin.com/wp-content/uploads/2017/07/logan.jpeg"
+                "https://www.thedigitalfix.com/wp-content/sites/thedigitalfix/2022/12/batman-big-part-in-dceu.jpg"
             ),
             SuperHero(
                 "Cyclops",
@@ -58,11 +16,68 @@ class SuperHeroProvider {
                 "https://i.pinimg.com/originals/24/a9/50/24a9503b4d68245584767b36c1e04202.jpg"
             ),
             SuperHero(
+                "Daredevil",
+                "Marvel",
+                "Matthew Michael Murdock",
+                "https://m.media-amazon.com/images/M/MV5BMTgzOTM4MTg1MV5BMl5BanBnXkFtZTcwODY1OTY4NA@@._V1_.jpg"
+            ),
+            SuperHero(
+                "Flash",
+                "DC",
+                "Jay Garrick",
+                "https://cloudfront-eu-central-1.images.arcpublishing.com/diarioas/KKMZUPHOWZAA7G5KOFV4AFUOW4.jpg"
+            ),
+            SuperHero(
                 "Green Arrow",
                 "DC",
                 "Oliver Queen",
                 "https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:600/h:442/q:75/https://bleedingcool.com/wp-content/uploads/2018/03/AR616b_0344b.jpg"
+            ),
+            SuperHero(
+                "Green Lantern",
+                "DC",
+                "Alan Scott",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/green-lantern.jpg"
+            ),
+            SuperHero(
+                "Spiderman",
+                "Marvel",
+                "Peter Parker",
+                "https://i.pinimg.com/originals/75/ee/62/75ee62dd2cf3ded21b9c15738b0654da.jpg"
+            ),
+
+            SuperHero(
+                "Storm",
+                "DC",
+                "Ororo Munroe",
+                "https://i.pinimg.com/originals/23/be/c4/23bec4b292721aa6cf93660a6b23e24d.jpg"
+            ),
+
+            SuperHero(
+                "Thor",
+                "Marvel",
+                "Thor Odinson",
+                "https://tonbarbier.com/app/uploads/2022/02/Thor.jpg"
+            ),
+            SuperHero(
+                "Wolverine",
+                "Marvel",
+                "James Howlett",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/logan.jpeg"
+            ),
+
+
+            SuperHero(
+                "Wonder Woman",
+                "DC",
+                "Princess Diana",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/wonder_woman.jpg"
             )
+
+
+
+
+
         )
     }
 }
